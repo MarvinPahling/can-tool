6ec2b1f58a4025c351d9b2221e3dff5f623ab6c5
Marvin Pahling <marvinpahling@gmail.com>
2026-08-23 13:03:47 +0200

    add Tauri dev tools

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -2,5 +2,15 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
+    #[cfg(debug_assertions)] // only enable instrumentation in development builds
+    let devtools = tauri_plugin_devtools::init();
+    let mut builder = tauri::Builder::default();
+    #[cfg(debug_assertions)]
+    {
+        builder = builder.plugin(devtools);
+    }
+    builder
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
     client_lib::run()
 }
